ee01d2383bb5ad99a8ae19a66ada483d5cb6d237
ismail / PIYO <ismail.karacaoglu@piyo.com.tr>
2019-03-31 22:45:41 +0300

    Webapi ilk Commit

---
+++ WebApiSampleGit/WebApiSampleGit/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiSampleGit.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApiSampleGit/WebApiSampleGit/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiSampleGit.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
